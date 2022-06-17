fc5474867f3f35401270b778788620385f8c41a5
Eugene Kovgan <jugin.kovgan87@gmail.com>
2022-06-18 00:02:02 +0300

    feat: begin (lesson 59) (06/18/2022 00:02:02)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import './App.css'
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
-import Navbar from './components/Navbar/Navbar'
-import Profile from './components/Profile/Profile'
-import { Routes, Route } from 'react-router-dom'
-import DialogsContainer from './components/Dialogs/DialogsContainer'
-import UsersContainer from './components/Users/UsersContainer'
-
+import './App.css';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Navbar from './components/Navbar/Navbar';
+import Profile from './components/Profile/Profile';
+import { Routes, Route } from 'react-router-dom';
+import DialogsContainer from './components/Dialogs/DialogsContainer';
+import UsersContainer from './components/Users/UsersContainer';
 
 const App = () => {
   return (
@@ -23,14 +22,13 @@ const App = () => {
               <Route path='/profile' element={<Profile />} />
               <Route path='/users' element={<UsersContainer />} />
             </Routes>
-
           </div>
         </div>
       </div>
 
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
