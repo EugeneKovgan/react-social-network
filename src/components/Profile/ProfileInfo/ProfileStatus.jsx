24bd0c41288406e352aef8f4c28b22b9c038186c
Eugene Kovgan <jugin.kovgan87@gmail.com>
2022-07-04 02:15:32 +0300

    feat: hook, useEffect (lesson 85) (07/04/2022 02:15:32)

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const ProfileStatus = (props) => {
   let [editMode, setEditMode] = useState(false);
   let [status, setStatus] = useState(props.status);
+
+  useEffect(() => {
+    setStatus(props.status);
+  }, [props.status]);
 
   let activateEditMode = () => {
     setEditMode(true);
